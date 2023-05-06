30002ac7c9fa00e763b7a59bd4337c7c7ce456e7
beratisahin <beratisahin94@gmail.com>
2023-05-07 00:55:18 +0300

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./Home.css";
+import NedenBercislina from "../home/neden/Neden.js";
+import Owl from "../slider/owlcarousel/Owl";
+import Klinik from "../klinik/Klinik.js";
+import Social from "../iletisim/Social";
+
+import TopluHizmet from "../topluhizmet/TopluHizmet.js";
+
+function Home() {
+  return (
+    <div>
+      <div className="row">
+        <h2
+          className="doktorlarTitle"
+          style={{
+            textAlign: "center",
+            fontSize: "x-large",
+            marginTop: "1rem",
+            color: "#b22e86",
+            fontFamily: "sans-serif",
+          }}
+        >
+          Kampanyalarımız Devam Etmektedir
+        </h2>
+        <Klinik />
+      </div>
+
+      <hr />
+      <div className="row">
+        <TopluHizmet />
+      </div>
+
+      <hr />
+      <div
+        style={{ margin: "auto", display: "block", justifyContent: "center" }}
+      >
+        <Social />
+      </div>
+      <hr />
+      <div className="row">
+        <h2
+          className="doktorlarTitle"
+          style={{
+            textAlign: "center",
+            fontSize: "x-large",
+            color: "#b22e86",
+            fontFamily: "sans-serif",
+          }}
+        >
+          Hizmetlerimiz
+        </h2>
+        <Owl />
+      </div>
+      <hr />
+
+      <div className="row m-2">
+        <div
+          className="col-lg-6 col-md-6 col-sm-6 col-12"
+          style={{ marginTop: "2.5rem" }}
+        >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d673.536092323585!2d35.484601409222044!3d38.725005275567405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x152b134692e3f88b%3A0x986547702b172bba!2sE&#39;liva%20Beauty%20Kayseri!5e0!3m2!1str!2str!4v1683409452023!5m2!1str!2str"
+            className="googleharita"
+            style={{
+              width: "100%",
+              height: "45rem",
+              frameBorder: "0",
+              allowFullScreen: "",
+              referrerpolicy: "no-referrer-when-downgrade",
+              ariaHidden: "false",
+              tabIndex: "0",
+              border: "2px #b22e86 solid",
+              paddingBottom: "0.5rem",
+              paddingTop: "0.5rem",
+            }}
+          >
+            {" "}
+          </iframe>
+        </div>
+
+        <div
+          className="col-lg-6 col-md-6 col-sm-6 col-12"
+          style={{ marginTop: "2.5rem" }}
+        >
+          <NedenBercislina />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
